b719c3b99aad0264fb24c342d83f94e8506f2e15
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-25 20:43:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function destructivelyPrependDriver(name)
   drivers.unshift(name)
 end
 
-function destructivelyRemoveLastDriver{()}
+function destructivelyRemoveLastDriver{
   drivers.pop() 
-end 
+}
 
 function destructivelyRemoveFirstDriver(){
   drivers.shift()
